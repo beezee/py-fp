7cebaf83f049ae749b0e7140b7b573ee5a772b63
beezee <brian.zeligson@gmail.com>
2019-10-07 10:22:32 -0400

    arrow composition

--- adt.py
+++ adt.py
@@ -33,11 +33,6 @@ class F6(Generic[T6]):
 class F7(Generic[T7]):
   run: T7 
 
-Foo = NamedTuple('Foo', [])
-Bar = NamedTuple('Bar', [])
-Baz = NamedTuple('Baz', [('name', str), ('num', int)])
-Quux = NamedTuple('Quux', [('num', int)])
-
 Sum2 = Union[F1[T1], F2[T2]]
 Sum3 = Union[F1[T1], F2[T2], F3[T3]]
 Sum4 = Union[F1[T1], F2[T2], F3[T3], F4[T4]]
@@ -51,7 +46,7 @@ class Fold2(ABC, Generic[T1, T2, Out]):
   @abstractmethod
   def f2(self, f: T2) -> Out: pass
 
-  def run(self, d: Sum2[T1, T2]) -> Out:
+  def __call__(self, d: Sum2[T1, T2]) -> Out:
     if isinstance(d, F1): 
       return self.f1(d.run)
     elif isinstance(d, F2): 
@@ -62,53 +57,61 @@ class Fold3(Fold2[T1, T2, Out], Generic[T1, T2, T3, Out]):
   @abstractmethod
   def f3(self, f: T3) -> Out: pass
 
-  def run(self, d: Sum3[T1, T2, T3]) -> Out:
+  def __call__(self, d: Sum3[T1, T2, T3]) -> Out:
     if isinstance(d, F3): 
       return self.f3(d.run)
     else: 
-      return super().run(d)
+      return super().__call__(d)
 
 class Fold4(Fold3[T1, T2, T3, Out], Generic[T1, T2, T3, T4, Out]):
   @abstractmethod
   def f4(self, f: T4) -> Out: pass
 
-  def run(self, d: Sum4[T1, T2, T3, T4]) -> Out:
+  def __call__(self, d: Sum4[T1, T2, T3, T4]) -> Out:
     if isinstance(d, F4): 
       return self.f4(d.run)
     else: 
-      return super().run(d)
+      return super().__call__(d)
 
 class Fold5(Fold4[T1, T2, T3, T4, Out], Generic[T1, T2, T3, T4, T5, Out]):
   @abstractmethod
   def f5(self, f: T5) -> Out: pass
 
-  def run(self, d: Sum5[T1, T2, T3, T4, T5]) -> Out:
+  def __call__(self, d: Sum5[T1, T2, T3, T4, T5]) -> Out:
     if isinstance(d, F5): 
       return self.f5(d.run)
     else: 
-      return super().run(d)
+      return super().__call__(d)
 
 class Fold6(Fold5[T1, T2, T3, T4, T5, Out], 
             Generic[T1, T2, T3, T4, T5, T6, Out]):
   @abstractmethod
   def f6(self, f: T6) -> Out: pass
 
-  def run(self, d: Sum6[T1, T2, T3, T4, T5, T6]) -> Out:
+  def __call__(self, d: Sum6[T1, T2, T3, T4, T5, T6]) -> Out:
     if isinstance(d, F6): 
       return self.f6(d.run)
     else: 
-      return super().run(d)
+      return super().__call__(d)
 
 class Fold7(Fold6[T1, T2, T3, T4, T5, T6, Out], 
             Generic[T1, T2, T3, T4, T5, T6, T7, Out]):
   @abstractmethod
   def f7(self, f: T7) -> Out: pass
 
-  def run(self, d: Sum7[T1, T2, T3, T4, T5, T6, T7]) -> Out:
+  def __call__(self, d: Sum7[T1, T2, T3, T4, T5, T6, T7]) -> Out:
     if isinstance(d, F7): 
       return self.f7(d.run)
     else: 
-      return super().run(d)
+      return super().__call__(d)
+
+Foo = NamedTuple('Foo', [])
+Bar = NamedTuple('Bar', [])
+Baz = NamedTuple('Baz', [('name', str), ('num', int)])
+Quux = NamedTuple('Quux', [('num', int)])
+
+FBBQ = Sum4[Foo, Bar, Baz, Quux]
+BQ = Sum2[Baz, Quux]
 
 class FoldPBQ(Fold2[Baz, Quux, str]):
   def f1(self, baz: Baz) -> str:
@@ -120,7 +123,7 @@ class FoldPrint(Fold3[Foo, Bar, Union[F1[Baz], F2[Quux]], str]):
   def f1(self, foo: Foo) -> str: return 'foo'
   def f2(self, bar: Bar) -> str: return 'bar'
   def f3(self, bq: Union[F1[Baz], F2[Quux]]) -> str:
-    return FoldPBQ().run(bq)
+    return FoldPBQ()(bq)
 
 class FoldP4(Fold4[Foo, Bar, Baz, Quux, Sum2[Baz, Quux]]):
   def f1(self, foo: Foo) -> F1[Baz]: return F1(Baz("foo", 1))
@@ -139,13 +142,22 @@ class FoldLen(Fold3[Foo, Bar, Baz, int]):
   def f3(self, baz: Baz) -> int:
     return len(baz.name) * baz.num
 
-print(FoldPrint().run(F1(Foo())))
-#print(FoldPrint().run(F3('baz')))
-print(FoldPrint().run(F3(F1(Baz('bazzz', 3)))))
 
-print(FoldP4().run(F1(Foo())))
-print(FoldP4().run(F12to34[Baz, Quux, Foo, Bar]().run(FoldP4().run(F2(Bar())))))
-print(FoldP4().run(F4(Quux(7))))
+class Compose(Generic[T1, T2, T3]):
+  def __init__(self, f1: Callable[[T1], T2], f2: Callable[[T2], T3]):
+    self.f1 = f1
+    self.f2 = f2
 
-print(FoldLen().run(F1(Foo())))
-print(FoldLen().run(F3(Baz('bazzz', 3))))
+  def __call__(self, t1: T1) -> T3:
+    return self.f2(self.f1(t1))
+
+print(FoldPrint()(F1(Foo())))
+#print(FoldPrint()(F3('baz')))
+print(FoldPrint()(F3(F1(Baz('bazzz', 3)))))
+
+print(FoldP4()(F1(Foo())))
+print(Compose(FoldP4(), F12to34[Baz, Quux, Foo, Bar]())(F2(Bar())))
+print(FoldP4()(F4(Quux(7))))
+
+print(FoldLen()(F1(Foo())))
+print(FoldLen()(F3(Baz('bazzz', 3))))
