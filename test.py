0c452692371bfbe9a3f4ec1c93883d7e32cbb5e0
beezee <brian.zeligson@gmail.com>
2019-10-06 23:54:51 -0400

    set up for multiple arities and demonstrate nesting. consider leak of positional types

--- test.py
+++ test.py
@@ -2,57 +2,77 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Callable, TypeVar, Generic, NamedTuple, Union
 
-LT = TypeVar('LT')
-MT = TypeVar('MT')
-RT = TypeVar('RT')
-T = TypeVar('T')
+T1 = TypeVar('T1')
+T2 = TypeVar('T2')
+T3 = TypeVar('T3')
+Out = TypeVar('Out')
 
 @dataclass
-class L(Generic[LT]):
-  run: LT
+class F1(Generic[T1]):
+  run: T1
 @dataclass
-class M(Generic[MT]):
-  run: MT
+class F2(Generic[T2]):
+  run: T2 
 @dataclass
-class R(Generic[RT]):
-  run: RT
+class F3(Generic[T3]):
+  run: T3 
 
 Foo = NamedTuple('Foo', [])
 Bar = NamedTuple('Bar', [])
 Baz = NamedTuple('Baz', [('name', str), ('num', int)])
+Quux = NamedTuple('Quux', [('num', int)])
 
-class Fold3(ABC, Generic[LT, MT, RT, T]):
+class Fold2(ABC, Generic[T1, T2, Out]):
   @abstractmethod
-  def left(self, l: L) -> T: pass
+  def f1(self, f: T1) -> Out: pass
   @abstractmethod
-  def middle(self, m: M) -> T: pass
-  @abstractmethod
-  def right(self, r: R) -> T: pass
+  def f2(self, f: T2) -> Out: pass
 
-  def run(self, d: Union[L[LT], M[MT], R[RT]]) -> T:
-    if isinstance(d, L): 
-      return self.left(d)
-    elif isinstance(d, M): 
-      return self.middle(d)
-    elif isinstance(d, R): 
-      return self.right(d)
+  def run(self, d: Union[F1[T1], F2[T2]]) -> Out:
+    if isinstance(d, F1): 
+      return self.f1(d.run)
+    elif isinstance(d, F2): 
+      return self.f2(d.run)
     else: assert False
 
-class FoldPrint(Fold3[Foo, Bar, Baz, str]):
-  def left(self, foo: L[Foo]) -> str: return 'foo'
-  def middle(self, bar: M[Bar]) -> str: return 'bar'
-  def right(self, baz: R[Baz]) -> str:
-    return baz.run.name * baz.run.num
+class Fold3(ABC, Generic[T1, T2, T3, Out]):
+  @abstractmethod
+  def f1(self, f: T1) -> Out: pass
+  @abstractmethod
+  def f2(self, f: T2) -> Out: pass
+  @abstractmethod
+  def f3(self, f: T3) -> Out: pass
+
+  def run(self, d: Union[F1[T1], F2[T2], F3[T3]]) -> Out:
+    if isinstance(d, F1): 
+      return self.f1(d.run)
+    elif isinstance(d, F2): 
+      return self.f2(d.run)
+    elif isinstance(d, F3): 
+      return self.f3(d.run)
+    else: assert False
+
+class FoldPBQ(Fold2[Baz, Quux, str]):
+  def f1(self, baz: Baz) -> str:
+    return baz.name * baz.num
+  def f2(self, quux: Quux) -> str:
+    return 'quux' * quux.num
+
+class FoldPrint(Fold3[Foo, Bar, Union[F1[Baz], F2[Quux]], str]):
+  def f1(self, foo: Foo) -> str: return 'foo'
+  def f2(self, bar: Bar) -> str: return 'bar'
+  def f3(self, bq: Union[F1[Baz], F2[Quux]]) -> str:
+    return FoldPBQ().run(bq)
 
 class FoldLen(Fold3[Foo, Bar, Baz, int]):
-  def left(self, foo: L[Foo]) -> int: return 1
-  def middle(self, bar: M[Bar]) -> int: return 2
-  def right(self, baz: R[Baz]) -> int:
-    return len(baz.run.name) * baz.run.num
+  def f1(self, foo: Foo) -> int: return 1
+  def f2(self, bar: Bar) -> int: return 2
+  def f3(self, baz: Baz) -> int:
+    return len(baz.name) * baz.num
 
-print(FoldPrint().run(L(Foo())))
+print(FoldPrint().run(F1(Foo())))
 #print(FoldPrint().run(R('baz')))
-print(FoldPrint().run(R(Baz('bazzz', 3))))
+print(FoldPrint().run(F3(F1(Baz('bazzz', 3)))))
 
-print(FoldLen().run(L(Foo())))
-print(FoldLen().run(R(Baz('bazzz', 3))))
+print(FoldLen().run(F1(Foo())))
+print(FoldLen().run(F3(Baz('bazzz', 3))))
