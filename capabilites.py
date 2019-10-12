f699fc61fab79407f8acb2ffabe7fe1ae0ef353d
beezee <brian.zeligson@gmail.com>
2019-10-11 20:14:48 -0400

    meh

--- capabilites.py
+++ capabilites.py
@@ -1,7 +1,10 @@
-from typing import Callable, NamedTuple, Tuple
+from typing import Callable, NamedTuple, Tuple 
 
 Read = NamedTuple('Read', [('read', Callable[[str], str])])
 Write = NamedTuple('Write', [('write', Callable[[str], None])])
+
+ProdRead = Read(input)
+ProdWrite = Write(print)
 
 def prompt(p: str, eff: Tuple[Read]) -> str:
   return eff[0].read(p)
@@ -14,6 +17,6 @@ def greet(eff: Tuple[Read, Write]) -> None:
   return _print('Hi, ' + name, (eff[1],))
 
 def greet_console() -> None:
-  greet((Read(input), Write(print)))
+  greet((ProdRead, ProdWrite))
 
 greet_console()
